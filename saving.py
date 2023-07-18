93d73e0f3c07fe7fbcff17a2f2f9f1e95a4ae91f
HeeSeungYoon <iyhs1858@gmail.com>
2023-07-18 12:49:39 +0900

    update

---
+++ saving.py
@@ -0,0 +1,17 @@
+import time
+from PyQt5.QtCore import *
+
+class Worker(QThread):
+    saving = pyqtSignal(int)
+
+    def __init__(self):
+        super().__init__()
+        
+    def run(self):
+        self.saving.emit(1)
+
+    def stop(self):
+        self.quit()    
+        self.wait(5000)
+        
+        
